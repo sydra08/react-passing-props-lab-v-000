ec387421273c6cf80709bf388c55cd19b9f18208
github username <schun08@gmail.com>
2018-05-16 08:59:03 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+// goal only have state maintained here
+// all App does so far is call FruitBasket
+
+// const App = () => <FruitBasket />;
+
+// turn App into a Component object
+// added states according to tests
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fruit: [],
+      filters: [],
+      currentFilter: null
+    }
+  }
+
+  componentDidMount() {
+    this.fetchFilters();
+    this.fetchFruit();
+  }
+
+  handleFilterChange = event => {
+    console.log('new filter: ', event.target.value);
+    this.setState({ currentFilter: event.target.value });
+  }
+
+  fetchFilters = () => {
+      fetch('/api/fruit_types')
+        .then(response => response.json())
+        .then(filters => this.setState({ filters }));
+    }
+
+  fetchFruit = () => {
+    fetch('/api/fruit')
+      .then(response => response.json())
+      .then(fruit => this.setState({ fruit }));
+  }
+
+  render() {
+    return (
+      <FruitBasket
+        updateFilterCallback={this.handleFilterChange} fruit={this.state.fruit}
+        filters={this.state.filters} currentFilter={this.state.currentFilter}
+      />
+    )
+  }
+}
 
 export default App;
--- src/components/FruitBasket.js
+++ src/components/FruitBasket.js
@@ -1,32 +1,61 @@
-import React, { Component }  from 'react';
+import React  from 'react';
 
 import Filter from './Filter';
 import FilteredFruitList from './FilteredFruitList.js';
 
-class FruitBasket extends Component {
-  constructor() {
-    super();
+// this calls upon Filter and FilteredFruitList
+// state consists of selectedFilter and filters
+// not sure why this.state.filters is necessary
 
-    this.state = {
-      filters: [],
-      selectedFilter: null
-    };
-  }
+// class FruitBasket extends Component {
+//   constructor() {
+//     super();
+//
+//     this.state = {
+//       filters: [],
+//       selectedFilter: null
+//     };
+//   }
+//
+//   // this passes the new filter state to Filter as a prop
+//   // this affects the state of FruitBasket, but in Filter it just handles the UI change when you select a different filter
+//   // and when that state change happens the new filter prop for FilteredFruitList gets passed
+//   // handleFilterChange = event => {
+//   //   console.log('new filter: ', event.target.value);
+//   //   this.setState({ selectedFilter: event.target.value });
+//   // }
+//
+//   render() {
+//     return (
+//       <div className="fruit-basket">
+//         <Filter handleChange={this.handleFilterChange} />
+//         <FilteredFruitList
+//           filter={this.state.selectedFilter} />
+//       </div>
+//     );
+//   }
+// }
 
-  handleFilterChange = event => {
-    console.log('new filter: ', event.target.value);
-    this.setState({ selectedFilter: event.target.value });
-  }
+const FruitBasket = ({ fruit, filters, currentFilter, updateFilterCallback}) => {
+  return (
+        <div className="fruit-basket">
+          <Filter
+            handleChange={updateFilterCallback}
+            filters={filters}
+          />
+          <FilteredFruitList
+            filter={currentFilter}
+            fruit={fruit}
+          />
+        </div>
+  )
+}
 
-  render() {
-    return (
-      <div className="fruit-basket">
-        <Filter handleChange={this.handleFilterChange} />
-        <FilteredFruitList
-          filter={this.state.selectedFilter} />
-      </div>
-    );
-  }
+FruitBasket.defaultProps = {
+  fruit: [],
+  filters: [],
+  currentFilter: null,
+  updateFilterCallback: function() {}
 }
 
 export default FruitBasket;
